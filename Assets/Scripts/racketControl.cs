aa5bf9e071d274c9213a57364ae6f66de14beb7a
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2021-11-22 14:49:15 +0300

    bounce and gravity

--- Assets/Scripts/racketControl.cs
+++ Assets/Scripts/racketControl.cs
@@ -6,20 +6,39 @@ public class racketControl : MonoBehaviour
 {
     Rigidbody rig;
     float mouseX, mouseY;
-
+    float bounceForce = 1;
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(Physics.gravity.x, -1, Physics.gravity.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        mouseX = Input.GetAxis("Mouse X") * Time.deltaTime;
-        mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime;
-        mouseX = Mathf.Clamp(mouseX, -0.3f, 0);
-        mouseY = Mathf.Clamp(mouseY, -0.135f, 0.135f);
+        /*mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);*/
+        
+    }
+    private void FixedUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * Time.fixedDeltaTime * 3;
+        mouseY += Input.GetAxis("Mouse Y") * Time.fixedDeltaTime * 3;
+        mouseY = Mathf.Clamp(mouseY, -0.3f, 0);
+        mouseX = Mathf.Clamp(mouseX, -0.135f, 0.135f);
+        //Debug.Log(Physics.gravity);
 
         transform.position = new Vector3(mouseX, mouseY, transform.position.z);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("degdi");
+        Rigidbody colRig = collision.collider.GetComponent<Rigidbody>();
+        if (colRig != null)
+        {
+            Vector3 vel = colRig.velocity;
+            vel.y = bounceForce;
+            colRig.velocity = vel;
+        }
     }
 }
